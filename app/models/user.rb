285ff12d968a6976c368b2fd856fc37fb340c799
Eric Suescun <edsuescun@gmail.com>
2019-03-28 17:44:25 -0500

    Implementada la nueva siembra de usuarios para test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
 
-	attr_accessor :remember_token	#This is a variable needed to hold the token for the login remember function
+	attr_accessor :remember_token, :activation_token
+	before_save   :downcase_email
+	before_create :create_activation_digest
 
-	before_save { self.email = email.downcase }
-	validates :name, presence: true, length: { maximum: 50}
+	validates :name,  presence: true, length: { maximum: 50 }
 	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   #Ese no tiene validacion de ".."
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }	#valid? adquiere un nuevo criterio: uniqueness, ademas se le pide que sea insensible a Case
@@ -37,5 +38,18 @@ class User < ApplicationRecord
 	def forget
 	    update_attribute(:remember_digest, nil)
 	end
+
+	private
+
+	    # Converts email to all lower-case.
+	    def downcase_email
+	      email.downcase!
+	    end
+
+	    # Creates and assigns the activation token and digest.
+	    def create_activation_digest
+	      self.activation_token  = User.new_token
+	      self.activation_digest = User.digest(activation_token)
+	    end
 
 end
