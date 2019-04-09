fc6e861ae7dc4cff62ab59b5956364ece9b3f376
Eric Suescun <edsuescun@gmail.com>
2019-04-09 12:41:29 -0500

    Fixed the fog configuration fo region in the carrier_wave.rb file, bad syntax

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,9 +5,9 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :S3_REGION			 => ENV['S3_REGION']
+      :region 				 => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_provider = 'fog/aws'
+    config.fog_provider 	 = 'fog/aws'
   end
 end
